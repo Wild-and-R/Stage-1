a5a9723970f167a7ab4ffed38d4f4a39d9c7d78e
Wildan Rahadian <ryan7300@gmail.com>
2025-12-11 14:55:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-//const express = require('express')
 import express from 'express';
 const app = express()
 const port = 5000
@@ -8,10 +7,23 @@ app.set('views', 'src/views');
 
 app.use("/assets",express.static("src/assets"));
 
-app.get('/', (req, res) => {
-  res.render("index")
-})
+app.get('/', home);
+app.get('/project',project);
+app.get('/project-detail',projectdetail);
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+function home(req, res) {
+  res.render("index")
+}
+
+function project(req, res) {
+  res.render("project")
+}
+
+function projectdetail(req, res) {
+  res.render("project-detail")
+}
